50847ad5c7f6c27cc2a3e0042a1bb6f78ae5c1de
vukamidzic <vuk.amidzic@gmail.com>
2026-02-02 12:47:34 +0100

    include Leaflet.js in frontend
    - include Svelte package for leaflet
    - write script for building and deploying website to S3 bucket

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+  <script>
+    import { onMount, onDestroy } from 'svelte';
+    import L from 'leaflet';
+    import 'leaflet/dist/leaflet.css';
+
+    let map;
+
+    onMount(async () => {
+      map = L.map('map').setView([44.8125, 20.4612], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(map);
+
+      L.marker([44.8125, 20.4612]).addTo(map);
+    });
+
+    onDestroy(() => {
+      if (map) map.remove();
+    });
+  </script>
+
+  <div id='map'></div>
+  <style>
+    #map {
+      width: 900px;
+      height: 600px;
+      margin: auto;
+    }
+  </style>
