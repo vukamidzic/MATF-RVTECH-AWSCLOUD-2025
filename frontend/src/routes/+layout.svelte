6f8356576329eb0ba5eae9e95ec5137b85c06e8b
vukamidzic <vuk.amidzic@gmail.com>
2026-02-03 20:01:32 +0100

    define styles for website elements

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,15 +2,4 @@
 	let { children } = $props();
 </script>
 
-<style>
-	#title {
-		text-align: center;
-		top: 0;
-		font-family: 'Times New Roman', Times, serif;
-		font-size: 50px;
-	}
-</style>
-
-<div id='title'>Brew & Bolt</div>
-
 {@render children()}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,20 +1,56 @@
   <style>
     :global(body) {
       display: grid;
-      place-content: center; /* Centers horizontally and vertically */
+      align-content: start;
+      justify-content: center;
       height: 100vh;
+      margin: 0;
+      background: radial-gradient(circle at center, #1631b8 40%, #d4d4d4 100%);
+      background-attachment: fixed;
     }
   
     #map {
       width: 900px;
       height: 600px;
       margin: auto;
+      border: 4px dashed black;
     }
 
     #loading {
-      margin: auto;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%); 
       width: 100px;
       height: 100px;
+    }
+
+    #title {
+      color: white;
+      font-size: 75px;
+      font-family: 'Courier New', Courier, monospace;
+      text-align: center;
+      margin-top: -10;
+    }
+
+    #find {
+      color: black;
+      box-shadow:10px;
+      background-color: white;
+      font-family: monospace;
+      font-size: 30px;
+      text-align: center;
+      transform: translate(50%, 0%);
+      width: 100px;
+    }
+
+    #find:hover {
+      background-color: grey;
+    }
+
+    #find:active {
+      background-color: black;
+      color: white;
     }
   </style>
   
@@ -33,10 +69,11 @@
     
       // Wait for Svelte to render the <div id="map">
       await tick(); 
-    
+
+      // Point to Belgrade
       map = L.map('map').setView([44.8125, 20.4612], 13);
     
-      // Add a tile layer (otherwise the map will be grey/blank)
+      // Add a tile layer
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap'
       }).addTo(map);
@@ -47,9 +84,16 @@
     onDestroy(() => { if (map) map.remove(); });
   </script>
 
+  <h2 id="title">Brew & Bolt</h2>
+
   {#if isReady}
     <div id='map'></div>
+    <!-- TODO: Define action (showing cafes near selected charger)  -->
+    <!-- TODO: Center button according to map -->
+    <button id="find">Show</button>
   {:else}
-    <div id="loading"><Circle/></div>
+    <div id="loading">
+      <Circle duration="1.5s" size="150" color="#FFFFFF" unit="px" />
+    </div>
   {/if}
   
